fee9f207a7abb95c90f774ac4bc8b29aa2ea476f
Renan Huanca <rhuanca@gmail.com>
2015-11-16 14:22:31 -0500

    now example is starting to work again.

--- main.c
+++ main.c
@@ -9,8 +9,7 @@
 #include "transport.h"
 
 
-void usage()
-{
+void usage() {
     printf("MQTT stdout subscriber\n");
     printf("Usage: stdoutsub topicname <options>, where options are:\n");
     printf("  --host <hostname> (default is localhost)\n");
@@ -24,45 +23,38 @@ void usage()
     exit(-1);
 }
 
-void cfinish(int sig)
-{
+void cfinish(int sig) {
     signal(SIGINT, NULL);
 }
 
-
-void stop_init(void)
-{
+void stop_init(void) {
     signal(SIGINT, cfinish);
     signal(SIGTERM, cfinish);
 }
 
-struct opts_struct
-{
-    char* clientid;
+struct opts_struct {
+    char *clientid;
     int nodelimiter;
-    char* delimiter;
+    char *delimiter;
     enum QoS qos;
-    char* username;
-    char* password;
-    char* host;
+    char *username;
+    char *password;
+    char *host;
     int port;
-    char* topic;
+    char *topic;
 } opts =
         {
-                (char*)"stdout-subscriber", 0, (char*)"\n", QOS2, NULL, NULL, (char*)"localhost", 1883, (char*) "test_topic"
+                (char *) "stdout-subscriber", 0, (char *) "\n", QOS2, NULL, NULL, (char *) "localhost", 1883,
+                (char *) "test_topic"
         };
 
 
-void getopts(int argc, char** argv)
-{
+void getopts(int argc, char **argv) {
     int count = 1;
 
-    while (count < argc)
-    {
-        if (strcmp(argv[count], "--qos") == 0)
-        {
-            if (++count < argc)
-            {
+    while (count < argc) {
+        if (strcmp(argv[count], "--qos") == 0) {
+            if (++count < argc) {
                 if (strcmp(argv[count], "0") == 0)
                     opts.qos = QOS0;
                 else if (strcmp(argv[count], "1") == 0)
@@ -75,51 +67,43 @@ void getopts(int argc, char** argv)
             else
                 usage();
         }
-        else if (strcmp(argv[count], "--host") == 0)
-        {
-            printf(">>> reading...host...\n");
+        else if (strcmp(argv[count], "--host") == 0) {
             if (++count < argc)
                 opts.host = argv[count];
             else
                 usage();
         }
-        else if (strcmp(argv[count], "--port") == 0)
-        {
+        else if (strcmp(argv[count], "--port") == 0) {
             if (++count < argc)
                 opts.port = atoi(argv[count]);
             else
                 usage();
         }
-        else if (strcmp(argv[count], "--clientid") == 0)
-        {
+        else if (strcmp(argv[count], "--clientid") == 0) {
             if (++count < argc)
                 opts.clientid = argv[count];
             else
                 usage();
         }
-        else if (strcmp(argv[count], "--username") == 0)
-        {
+        else if (strcmp(argv[count], "--username") == 0) {
             if (++count < argc)
                 opts.username = argv[count];
             else
                 usage();
         }
-        else if (strcmp(argv[count], "--password") == 0)
-        {
+        else if (strcmp(argv[count], "--password") == 0) {
             if (++count < argc)
                 opts.password = argv[count];
             else
                 usage();
         }
-        else if (strcmp(argv[count], "--delimiter") == 0)
-        {
+        else if (strcmp(argv[count], "--delimiter") == 0) {
             if (++count < argc)
                 opts.delimiter = argv[count];
             else
                 opts.nodelimiter = 1;
         }
-        else if (strcmp(argv[count], "--topic") == 0)
-        {
+        else if (strcmp(argv[count], "--topic") == 0) {
             if (++count < argc)
                 opts.topic = argv[count];
             else
@@ -130,8 +114,7 @@ void getopts(int argc, char** argv)
 
 }
 
-int main(int argc, char** argv)
-{
+int main(int argc, char **argv) {
     int rc = 0;
     unsigned char buf[100];
     unsigned char readbuf[100];
@@ -139,7 +122,7 @@ int main(int argc, char** argv)
 
     int len = 0;
 
-    char* payload = "hola de nuevo....ops...111.. este mensage no quiere ser publicado...";
+    char *payload = "message";
     int payloadlen = strlen(payload);
     int mysock = 0;
 
@@ -153,7 +136,7 @@ int main(int argc, char** argv)
 
     getopts(argc, argv);
 
-    printf("host: %s\n", opts.host );
+    printf("host: %s\n", opts.host);
     printf("topic: %s\n", opts.topic);
 
     signal(SIGINT, cfinish);
@@ -162,12 +145,8 @@ int main(int argc, char** argv)
     mysock = transport_open(opts.host, opts.port);
 
 
-    if(mysock < 0)
+    if (mysock < 0)
         return mysock;
-
-
-    printf("hello----------------2\n");
-
 
     MQTTPacket_connectData data = MQTTPacket_connectData_initializer;
     data.willFlag = 0;
@@ -187,7 +166,7 @@ int main(int argc, char** argv)
 
     topicString.cstring = "test";
 
-    len = MQTTSerialize_publish(buf, buflen, 0, 0, 0, 0, topicString, (unsigned char*)payload, payloadlen);
+    len = MQTTSerialize_publish(buf, buflen, 0, 0, 0, 0, topicString, (unsigned char *) payload, payloadlen);
 
     printf("len = %d\n", len);
     printf("mysock = %d\n", mysock);
